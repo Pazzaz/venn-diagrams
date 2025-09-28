7ab2ba673e000f42725909d2c7c648da4121b334
Pazzaz <pazzaz.sundqvist@gmail.com>
2025-09-28 08:32:32 +0200

    Move `Diagram` to `lib`

---
+++ src/direction.rs
@@ -0,0 +1,57 @@
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum Direction {
+    Horizontal { y: usize, x1: usize, x2: usize },
+    Vertical { x: usize, y1: usize, y2: usize },
+}
+
+use Direction::*;
+
+impl Direction {
+    pub fn len(&self) -> usize {
+        match self {
+            &Horizontal { x1, x2, .. } => x1.abs_diff(x2),
+            &Vertical { y1, y2, .. } => y1.abs_diff(y2),
+        }
+    }
+}
+
+pub fn endpoints(d: &Direction) -> ((usize, usize), (usize, usize)) {
+    match d {
+        &Horizontal { y, x1, x2 } => ((x1, y), (x2, y)),
+        &Vertical { x, y1, y2 } => ((x, y1), (x, y2)),
+    }
+}
+
+pub fn from_endpoints((x1, y1): (usize, usize), (x2, y2): (usize, usize)) -> Direction {
+    if x1 == x2 {
+        Vertical { x: x1, y1, y2 }
+    } else if y1 == y2 {
+        Horizontal { y: y1, x1, x2 }
+    } else {
+        panic!("Invalid endpoints");
+    }
+}
+
+pub fn connected(a: &Direction, b: &Direction) -> bool {
+    assert!(a != b);
+    let (p1, p2) = endpoints(a);
+    let (p3, p4) = endpoints(b);
+    p1 == p3 || p1 == p4 || p2 == p3 || p2 == p4
+}
+
+pub fn combine(a: &Direction, b: &Direction) -> Option<Direction> {
+    if !connected(a, b) {
+        return None;
+    }
+
+    match (a, b) {
+        (&Horizontal { x1, x2, y }, &Horizontal { x1: x3, x2: x4, .. }) => {
+            Some(Horizontal { y, x1: x1.min(x3), x2: x2.max(x4) })
+        }
+        (Horizontal { .. }, Vertical { .. }) => None,
+        (Vertical { .. }, Horizontal { .. }) => None,
+        (&Vertical { x, y1, y2 }, &Vertical { y1: y3, y2: y4, .. }) => {
+            Some(Vertical { x, y1: y1.min(y3), y2: y2.max(y4) })
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate static_assertions;
 
+mod direction;
+
+pub mod diagram;
 pub mod venn;
 
 pub type Polyomino<const X: usize, const Y: usize> = [[bool; X]; Y];
