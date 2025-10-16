d4bce6466f30df1694eaa3a295bb973866a6e932
Pazzaz <pazzaz.sundqvist@gmail.com>
2025-10-16 17:06:09 +0200

    Add some documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+//! # Venn Diagrams
+//!
+//! A library to work with [Venn diagrams][wp-venn], specifically those
+//! consisting of [polyominos][wp-poly].
+//!
+//! # Research
+//! Parts of this library is based on the following research:
+//! - "Minimum Area Venn Diagrams Whose Curves are Polyominoes"; Stirling Chow, Frank Ruskey; [arxiv:0603068](https://arxiv.org/abs/math/0603068)
+//! - "Minimum area polyomino Venn diagrams"; Bette Bultena, Matthew Klimesh, Frank Ruskey; [doi:10.20382/jocg.v3i1a8](https://doi.org/10.20382/jocg.v3i1a8)
+//!
+//! [wp-venn]: https://en.wikipedia.org/wiki/Venn_diagram
+//! [wp-poly]: https://en.wikipedia.org/wiki/Polyomino
+
 mod direction;
 
 pub mod diagram;
