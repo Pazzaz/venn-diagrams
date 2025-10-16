10cea10eb6758e144be29cbaf30725ae1b6aea11
Pazzaz <pazzaz.sundqvist@gmail.com>
2025-10-16 15:32:56 +0200

    Add `Polyomino` and implement som traits

--- src/polyomino.rs
+++ src/polyomino.rs
@@ -1,3 +1,5 @@
+use std::ops::{Index, IndexMut};
+
 #[derive(Debug, Clone, Copy)]
 pub struct ConstPolyomino<const X: usize, const Y: usize> {
     pub(crate) values: [[bool; X]; Y],
@@ -24,5 +26,84 @@ impl<const X: usize, const Y: usize> ConstPolyomino<X, Y> {
             y += 1;
         }
         out
+    }
+}
+
+impl<const X: usize, const Y: usize> Index<(usize, usize)> for ConstPolyomino<X, Y> {
+    type Output = bool;
+
+    fn index(&self, (x, y): (usize, usize)) -> &Self::Output {
+        &self.values[y][x]
+    }
+}
+
+impl<const X: usize, const Y: usize> IndexMut<(usize, usize)> for ConstPolyomino<X, Y> {
+    fn index_mut(&mut self, (x, y): (usize, usize)) -> &mut Self::Output {
+        &mut self.values[y][x]
+    }
+}
+
+#[derive(Debug)]
+pub struct Polyomino {
+    x: usize,
+    y: usize,
+    values: Vec<bool>,
+}
+
+impl Polyomino {
+    pub fn x(&self) -> usize {
+        self.x
+    }
+
+    pub fn y(&self) -> usize {
+        self.y
+    }
+
+    pub fn empty(x: usize, y: usize) -> Self {
+        Self { x, y, values: vec![false; x * y] }
+    }
+
+    pub fn get(&self, x: usize, y: usize) -> bool {
+        self.values[y * self.x + x]
+    }
+
+    pub fn get_mut(&mut self, x: usize, y: usize) -> &mut bool {
+        &mut self.values[y * self.x + x]
+    }
+}
+
+impl Clone for Polyomino {
+    fn clone(&self) -> Self {
+        Self { x: self.x, y: self.y, values: self.values.clone() }
+    }
+
+    fn clone_from(&mut self, source: &Self) {
+        self.x = source.x;
+        self.y = source.y;
+        self.values.clone_from(&source.values);
+    }
+}
+
+impl Index<(usize, usize)> for Polyomino {
+    type Output = bool;
+
+    fn index(&self, (x, y): (usize, usize)) -> &Self::Output {
+        &self.values[y * self.x + x]
+    }
+}
+
+impl IndexMut<(usize, usize)> for Polyomino {
+    fn index_mut(&mut self, (x, y): (usize, usize)) -> &mut Self::Output {
+        &mut self.values[y * self.x + x]
+    }
+}
+
+impl<const X: usize, const Y: usize> From<ConstPolyomino<X, Y>> for Polyomino {
+    fn from(value: ConstPolyomino<X, Y>) -> Self {
+        Polyomino {
+            x: X,
+            y: Y,
+            values: value.values.into_iter().flat_map(IntoIterator::into_iter).collect(),
+        }
     }
 }
