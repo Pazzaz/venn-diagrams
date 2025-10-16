f641ee900d0faa0ba9e9fd26735e23949820a99c
Pazzaz <pazzaz.sundqvist@gmail.com>
2025-10-16 21:04:47 +0200

    Use `insta` for tests

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ![Example diagram](image.svg)
 
 Program to illustrate data with Venn diagrams. Originally created to show possible coalitions in parliamentary democracies.
+
+# Development
+This library uses [Insta](https://docs.rs/insta/latest/insta/index.html) for snapshot tests.
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -3,7 +3,12 @@ use venn_diagrams::{
     venn,
 };
 
-fn main() {
+#[test]
+fn parliament_test() {
+    fn normalize<const N: usize>(values: [f64; N]) -> [f64; N] {
+        let sum: f64 = values.iter().sum();
+        values.map(|x| x / sum)
+    }
     let colors_s =
         ["#EE2020", "#DDDD00", "#1B49DD", "#AF0000", "#009933", "#231977", "#83CF39", "#6BB7EC"];
     let values = normalize([107.0, 73.0, 68.0, 24.0, 24.0, 19.0, 18.0, 16.0]);
@@ -21,10 +26,5 @@ fn main() {
     };
 
     let svg = diagram.to_svg();
-    svg::save("image.svg", &svg).unwrap();
-}
-
-fn normalize<const N: usize>(values: [f64; N]) -> [f64; N] {
-    let sum: f64 = values.iter().sum();
-    values.map(|x| x / sum)
+    insta::assert_binary_snapshot!(".svg", svg.to_string().as_bytes().into_iter().cloned().collect());
 }
