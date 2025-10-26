e98fe84cf8edb707c90e6bda616990d7bac21cc1
Pazzaz <pazzaz.sundqvist@gmail.com>
2025-10-26 12:19:40 +0100

    Add test for optimizing `EIGHT`

--- tests/optimizing.rs
+++ tests/optimizing.rs
@@ -15,3 +15,13 @@ fn five() {
     config.offset_method = OffsetMethod::Optimizing;
     test_venn("five.svg", &venn::FIVE.into(), &values, &colors, &mut config);
 }
+
+#[test]
+#[ignore = "expensive"]
+fn eight() {
+    let colors = COLORS;
+    let values = normalize(&VALUES);
+    let mut config = DiagramConfig::default();
+    config.offset_method = OffsetMethod::Optimizing;
+    test_venn("eight.svg", &venn::EIGHT.into(), &values, &colors, &mut config);
+}
