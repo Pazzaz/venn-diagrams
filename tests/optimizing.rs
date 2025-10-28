21b4ec2040ed32da2f18859da58322af9c25de55
Pazzaz <pazzaz.sundqvist@gmail.com>
2025-10-28 17:02:08 +0100

    Add more `optimizing` tests

--- tests/optimizing.rs
+++ tests/optimizing.rs
@@ -10,6 +10,27 @@ use crate::common::test_venn;
 mod common;
 
 #[test]
+fn two() {
+    let mut config = DiagramConfig::default();
+    config.offset_method = OffsetMethod::Optimizing;
+    test_venn("two.svg", constants::TWO.into(), &mut config);
+}
+
+#[test]
+fn three() {
+    let mut config = DiagramConfig::default();
+    config.offset_method = OffsetMethod::Optimizing;
+    test_venn("three.svg", constants::THREE.into(), &mut config);
+}
+
+#[test]
+fn four() {
+    let mut config = DiagramConfig::default();
+    config.offset_method = OffsetMethod::Optimizing;
+    test_venn("four.svg", constants::FOUR.into(), &mut config);
+}
+
+#[test]
 fn five() {
     let mut config = DiagramConfig::default();
     config.offset_method = OffsetMethod::Optimizing;
